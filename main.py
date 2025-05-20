b6ed3304b397350024be9ca2da173d55f7dc25a6
David Gomes Santa Rosa <dgsrfut07@gmail.com>
2025-05-19 21:27:05 -0300

    Primeiro Commit

---
+++ main.py
@@ -0,0 +1,54 @@
+while True:
+    user_action = input("Type add, show, edit, complete or exit: ")
+    user_action = user_action.strip()
+
+    if 'add' in user_action:
+        todo = user_action[4:].strip() + '\n'
+
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
+
+        todos.append(todo)
+
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
+
+    elif 'show' in user_action:
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
+
+        new_todos = [item.strip("\n") for item in todos] # Remover as quebras de linha extras
+
+        for index, item in enumerate(new_todos):
+            row = f"{index + 1}. {item}"
+            print(row)
+    elif 'edit' in user_action:
+        number = int(user_action[5:])
+        number = number - 1
+
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
+
+        new_todo = input("Enter a new todo: ")
+        todos[number] = new_todo
+
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
+
+    elif 'complete' in user_action:
+        number = int(user_action[9:])
+
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
+
+        todo_to_remove = todos[number - 1].title()
+        todos.pop(number - 1)
+
+        with open('todos.txt', 'w') as file:
+            file.writelines(todos)
+
+        print(f"Todo {todo_to_remove.strip("\n")} was removed from the list")
+    elif 'exit' in user_action:
+        break
+    else:
+        print('Choose a Valid Option')
