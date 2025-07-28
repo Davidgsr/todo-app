2a3c9e657aa2b31f3edd52ce6cd1c6ee090d0610
David Gomes Santa Rosa <dgsrfut07@gmail.com>
2025-07-28 16:33:58 -0300

    Trabalhando com biblioteca Streamlit para desenvolver projetos web

--- functions.py
+++ functions.py
@@ -1,13 +1,21 @@
-FILEPATH = 'todo-app/todos.txt'
+import os
 
+FILEPATH = 'todos.txt'
 
 def get_todos(filepath=FILEPATH) -> list:
-    """ Read a text file and return the list of to-do items. """
+    """Read a text file and return the list of to-do items."""
+
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
+    
+    todos_local = [todo.strip() + '\n' for todo in todos_local]
+
     return todos_local
 
+
 def write_todos(todos_arg, filepath=FILEPATH):
-    """ Write the to-do items list in the text file. """
+    """Write the to-do items list in the text file."""
+
+
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
